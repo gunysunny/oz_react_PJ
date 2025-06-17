b4e8448c823afd98cdfffeb2a40de372681694e2
gunysunny <mikan31@naver.com>
2025-06-17 15:38:26 +0900

    refactor: App.jsx 라우트 구조를 Layout 기반으로 리팩터링
    - "/" 진입 시 Layout을 최상위로, 내부에서 MovieList/Detail 페이지 렌더링되도록 구조 변경
    - Layout 내부에서 <Outlet>으로 자식 라우트 컴포넌트 표시

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+// react-router-dom에서 라우터 관련 컴포넌트(import)
+// BrowserRouter: 전체 앱에 라우팅 기능을 적용(히스토리, 주소 관리)
+// Routes: 여러 Route를 그룹화하는 컴포넌트
+// Route: 특정 경로(path)에 특정 컴포넌트(element)를 렌더링
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// 영화 목록 페이지 컴포넌트 import
 import MovieList from "./component/movielist/MovieList";
+// 영화 상세 페이지 컴포넌트 import
 import MovieDetail from "./component/moviedetail/MovieDetail";
+import Layout from "./component/layout/Layout";
 
+// App 컴포넌트: 전체 애플리케이션의 라우팅 구조를 담당
 function App() {
   return (
+    // BrowserRouter로 전체 앱을 감싸서 라우팅 기능 활성화(히스토리, URL 동기화 등)
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MovieList />} />
-        <Route path="/detail/:id" element={<MovieDetail />} />
+        <Route path="/" element={<Layout />}>
+          {/* Layout이 렌더링되고, 그 안에서 Outlet에 아래 컴포넌트가 들어감 */}
+          <Route index element={<MovieList />} />
+          <Route path="detail/:id" element={<MovieDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
+
+// App 컴포넌트 export(기본 내보내기)
 export default App;
