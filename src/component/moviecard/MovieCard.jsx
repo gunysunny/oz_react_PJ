0f9eddb4f159295b91348d81ca3fbdb5264630c7
gunysunny <mikan31@naver.com>
2025-06-17 13:27:25 +0900

    feat: MovieCard 컴포넌트 생성

---
+++ src/component/moviecard/MovieCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function MovieCard({ title, poster_path, vote_average }) {
+    return (
+        <div className="bg-white shadow rounded-lg p-4 w-60 flex flex-col items-start hover:scale-105 transition cursor-pointer">
+        <img
+            src={poster_path}
+            alt={title}
+            className="w-full h-80 object-cover rounded mb-3"
+        />
+        <h3 className="text-lg font-semibold mb-1 text-center">{title}</h3>
+        <span className="text-gray-500 font-semibold text-sm">
+            평점: {vote_average}
+        </span>
+        </div>
+    );
+}
