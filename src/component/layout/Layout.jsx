124ee8ed51198e8a42d2af2608080a9d670aadf1
gunysunny <mikan31@naver.com>
2025-06-20 16:40:57 +0900

    feat(layout): add search state and context for search props
    - Layout 컴포넌트에서 useState로 search 상태를 관리하도록 추가
    - Outlet context를 통해 하위 컴포넌트에 search 전달 구조 구현

--- src/component/layout/Layout.jsx
+++ src/component/layout/Layout.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../navbar/NavBar";
 import { Outlet } from "react-router-dom";
 
 // 모든 페이지에서 NavBar + 각 페이지 콘텐츠를 동시에 보여주는 Layout 컴포넌트
 function Layout() {
+    const [search, setSearch] = useState(""); // 검색 상태 Layout에서 관리
     return (
         <div>
-            <NavBar />
+            <NavBar onSearch={setSearch} />
             {/* Outlet 자리에 각 Route에 매칭된 컴포넌트가 렌더링됨 */}
-            <Outlet />
+            <Outlet context={{ search }} />
         </div>
     );
 }
