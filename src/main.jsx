71a00b1f09dd6336499fd05857a9fce5a9b7e195
gunysunny <mikan31@naver.com>
2025-07-02 16:17:58 +0900

    chore(main): UserProvider로 App 컴포넌트 감싸기
    - Context API 기반 전역 유저 상태 관리를 위해 UserProvider 적용

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import { SupabaseProvider } from "../supabase/context";
+import { UserProvider } from "./context/UserContext"; // ← 추가!
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SupabaseProvider>
-      <App />
+      <UserProvider>       {/* ← 이렇게 감싸주기 */}
+        <App />
+      </UserProvider>
     </SupabaseProvider>
   </StrictMode>,
 )
