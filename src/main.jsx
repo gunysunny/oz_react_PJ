1d805bb4f532d260a82c7fa82fa144dabf8ae228
gunysunny <mikan31@naver.com>
2025-07-04 20:12:18 +0900

    feat: 위시리스트 전역 관리 위한 WishlistProvider 적용

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './App.css'
 import { SupabaseProvider } from "../supabase/context";
 import { UserProvider } from "./context/UserContext"; // ← 추가!
-
 import App from './App.jsx'
+import { WishlistProvider } from './context/WishlistContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SupabaseProvider>
-      <UserProvider>       {/* ← 이렇게 감싸주기 */}
-        <App />
+      <UserProvider>
+        <WishlistProvider>
+          <App />
+        </WishlistProvider>
       </UserProvider>
     </SupabaseProvider>
-  </StrictMode>,
+  </StrictMode>
 )
